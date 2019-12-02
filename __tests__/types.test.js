f90dd6e5f8737dba7144cc2ab2fd2d67b2ee51be
DannySuarez <suarezd10@gmail.com>
2019-12-02 14:35:13 -0800

    Add: Cast to boolean

--- __tests__/types.test.js
+++ __tests__/types.test.js
@@ -2,8 +2,12 @@ const {
   isNumber,
   isString,
   isBoolean,
+  isArray,
   isObject,
+  isFunction,
   castToNumber,
+  castToString,
+  castToBoolean,
   getCaster
 } = require('../lib/types.js');
 
@@ -37,6 +41,15 @@ describe('validator module', () => {
       expect(isBoolean(() => {})).toBeFalsy();
     });
 
+    it('isArray', () => {
+      expect(isArray(3)).toBeFalsy();
+      expect(isArray('hi')).toBeFalsy();
+      expect(isArray(['a', 'b'])).toBeTruthy();
+      expect(isArray({})).toBeFalsy();
+      expect(isArray(() => {})).toBeFalsy();
+      expect(isArray(true)).toBeFalsy();
+    });
+
     it('object', () => {
       expect(isObject(3)).toBeFalsy();
       expect(isObject('hi')).toBeFalsy();
@@ -44,6 +57,15 @@ describe('validator module', () => {
       expect(isObject({})).toBeTruthy();
       expect(isObject(() => {})).toBeFalsy();
       expect(isObject(true)).toBeFalsy();
+    });
+
+    it('function', () => {
+      expect(isFunction(3)).toBeFalsy();
+      expect(isFunction('hi')).toBeFalsy();
+      expect(isFunction([])).toBeFalsy();
+      expect(isFunction({})).toBeFalsy();
+      expect(isFunction(() => {})).toBeTruthy();
+      expect(isFunction(true)).toBeFalsy();
     });
   });
 
@@ -53,6 +75,21 @@ describe('validator module', () => {
       expect(castToNumber('3')).toEqual(3);
       expect(castToNumber(true)).toEqual(1);
       expect(castToNumber(false)).toEqual(0);
+    });
+
+    it('can cast to string', () => {
+      expect(castToString(3)).toEqual('3');
+      expect(castToString('3')).toEqual('3');
+      expect(castToString(true)).toEqual('true');
+      expect(castToString(false)).toEqual('false');
+    });
+
+    it('can cast to boolean', () => {
+      expect(castToBoolean(true)).toEqual(true);
+      expect(castToBoolean(false)).toEqual(false);
+      expect(castToBoolean(0)).toEqual(false);
+      expect(castToBoolean(3)).toEqual(true);
+      expect(castToBoolean('3')).toEqual(true);
     });
 
     it('throws if value is not castable to number', () => {
